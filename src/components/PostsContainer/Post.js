92d973ad0ceee4a1e595534253d5902a9eb8e22f
Kevin <knphillips01@gmail.com>
2020-01-21 16:14:47 -0600

    complated Postspage

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -9,7 +9,7 @@ import "./Posts.css";
 
 const Post = props => {
   // set up state for the likes
- const [likes, setLikes ] = useState(props.posts.like);
+ const [likes, setLikes ] = useState(props.post.likes);
 
  const increaseLikes = () => {
    setLikes(likes => likes + 1);
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,16 +1,21 @@
 //Complete the necessary code in this file
-import React from "react";
+import React, { useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = props => {
+  console.log(props)
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here */}
+      {props.postData.map(p => (
+        <Post 
+          key={p.imageUrl} 
+          post={p} 
+        />
+      ))}
     </div>
   );
 };
 
 export default PostsPage;
-
